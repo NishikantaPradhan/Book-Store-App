0f6d25b1c9bb53f0e5f0ff168607fb90e63f2cbf
NishikantaPradhan <nishikanta9266@gmail.com>
2024-08-28 17:59:33 +0530

    My BookStore App Project

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import mongoose from "mongoose";
+import { PORT } from "./config.js";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default Of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('welcome To MERN Stack Tutorial')
+});
+
+
+
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect("mongodb://localhost:27017/bookstore")
+  .then(() => {
+    console.log(`App connected to database`);
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+    
+  })
+  .catch((error) => {
+    console.log(error);
+  })
